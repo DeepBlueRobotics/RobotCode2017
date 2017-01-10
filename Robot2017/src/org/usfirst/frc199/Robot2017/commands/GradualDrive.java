80afd5a110002f54c97c6f3ec0162e77342407b6
kevinzwang <kevinzhiwang@gmail.com>
2017-01-09 16:00:53 -0800

    initial drivetrain functionality
    finished:
    - teleop drive
    - gradual drive
    - toggle drive type

--- Robot2017/src/org/usfirst/frc199/Robot2017/commands/GradualDrive.java
+++ Robot2017/src/org/usfirst/frc199/Robot2017/commands/GradualDrive.java
@@ -40,6 +40,7 @@ public class GradualDrive extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.drivetrain.gradualDrive();
     }
 
     // Make this return true when this Command no longer needs to run execute()
