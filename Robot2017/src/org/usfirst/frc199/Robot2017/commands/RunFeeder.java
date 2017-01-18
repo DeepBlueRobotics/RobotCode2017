759e8143b5a62e44f136baafa9155a7c9906b3fa
caengineers <caengineers@ca-studentlabs.org>
2017-01-17 17:35:16 -0800

    added method shooterMotorStalled to shooter subsystem and implemented

--- Robot2017/src/org/usfirst/frc199/Robot2017/commands/RunFeeder.java
+++ Robot2017/src/org/usfirst/frc199/Robot2017/commands/RunFeeder.java
@@ -29,7 +29,7 @@ public class RunFeeder extends Command {
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-		Robot.shooter.feeder(speed);
+		Robot.shooter.runFeederMotor(speed);
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
@@ -39,7 +39,7 @@ public class RunFeeder extends Command {
 
 	// Called once after isFinished returns true
 	protected void end() {
-		Robot.shooter.feeder(0);
+		Robot.shooter.runFeederMotor(0);
 	}
 
 	// Called when another command which requires one or more of the same
