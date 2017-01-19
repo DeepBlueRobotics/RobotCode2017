5092e988a9d06863833aa9219b360aa5903ab372
caengineers <caengineers@ca-studentlabs.org>
2017-01-19 15:04:31 -0800

    added last year's motion profiling code and added a max jerk constraint

---
+++ Robot2017/src/org/usfirst/frc199/Robot2017/motion/Main.java
@@ -0,0 +1,116 @@
+package org.usfirst.frc199.Robot2017.motion;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.Timer;
+
+public class Main {
+	
+	static Path p;
+	static Trajectory trajectory;
+	
+	public static void main(String[] args) {
+		startPathDemo();
+	}
+	
+	public static void startPathDemo() {
+		p = new Path(25,25,50,50,0,0,2);
+		JFrame frame = new JFrame("Pathfinder");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500, 600);
+		JTextField f1 = new JTextField("0");
+		JTextField f2 = new JTextField("0");
+		JTextField f3 = new JTextField("2");
+		@SuppressWarnings("serial")
+		class MyPanel extends JPanel {
+			ArrayList<Double> xarray = new ArrayList<>();
+			ArrayList<Double> yarray = new ArrayList<>();
+			@Override
+			public void paint(Graphics g) {
+				for(double s = 0; s<=1; s+=.002) {
+					double x = p.getX(s)*10;
+					double y = p.getY(s)*10;
+					y = getHeight()-y;
+					g.setColor(Color.CYAN);
+					g.fillRect((int)x-1, (int)y-1, 3, 3);				
+				}
+				for(int i=0; i<xarray.size(); i++) {
+					double x = xarray.get(i);
+					double y = yarray.get(i);
+					double y2 = getHeight()-y*10;
+					double x2 = x*10;
+					g.setColor(Color.MAGENTA);
+					g.fillRect((int)x2-1, (int)y2-1, 3, 3);
+				}
+			}
+			Timer tim;
+			public void draw() {
+				xarray.clear();
+				yarray.clear();
+				tim = new Timer(1, new ActionListener(){
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						if(p.getS(l)<1)doStuff();
+						else tim.stop();
+					}
+				});
+				l = 0;
+				theta = p.getTheta(0);
+				x = p.getX(0);
+				y = p.getY(0);
+				dt = .1;
+				tim.start();
+			}
+			double l = 0, theta = p.getTheta(0), x = p.getX(0), y = p.getY(0), dt = .1;
+			
+			public void doStuff() {
+				// The motion profiling part
+				int i = trajectory.getCurrentIndex(l);
+				double v = trajectory.getV(i);
+				l+=v*dt;
+				double w = trajectory.getW(i);
+				theta += w * dt;
+				x += (v * Math.sin((Math.toRadians(theta)))) * dt;
+				y += (v * Math.cos((Math.toRadians(theta)))) * dt;
+				xarray.add(x);
+				yarray.add(y);
+				repaint();
+			}
+		};
+		MyPanel panel = new MyPanel();
+		panel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent me) {
+				double x = me.getX()/10;
+				double y = (panel.getHeight()-me.getY())/10;
+				double angle0 = Double.parseDouble(f1.getText());
+				double angle1 = Double.parseDouble(f2.getText());
+				double k = Double.parseDouble(f3.getText());
+				p = new Path(25,25,x,y,angle0,angle1,k);
+				trajectory = new Trajectory(p, .01, .01, 10, .8, 10, .8, 1007);
+				panel.draw();
+			}
+		});
+		panel.setPreferredSize(new Dimension(500, 500));
+		f1.setPreferredSize(new Dimension(100, 25));
+		f2.setPreferredSize(new Dimension(100, 25));
+		f3.setPreferredSize(new Dimension(100, 25));
+		frame.setLayout(new FlowLayout());
+		frame.add(panel);
+		frame.add(f1);
+		frame.add(f2);
+		frame.add(f3);
+		frame.setVisible(true);
+	}
+}
---
+++ Robot2017/src/org/usfirst/frc199/Robot2017/motion/Trajectory.java
@@ -0,0 +1,191 @@
+package org.usfirst.frc199.Robot2017.motion;
+
+/**
+ * Generates velocity values for a set of points along a path.
+ */
+public class Trajectory {
+
+	// The path used to generate the trajectory
+	private final Path path;
+	// The constraints on the trajectory
+	private final double vmax, amax, wmax, alphamax, jerkmax;
+	// The set of calculated target velocity values
+	private final double[] velocities;
+	
+	private final double[] acceleration;
+
+	/**
+	 * Generates a trajectory that follows a given path under the given
+	 * constraints
+	 * 
+	 * @param path
+	 *            - the path to follow
+	 * @param v0
+	 *            - the initial velocity
+	 * @param v1
+	 *            - the final velocity
+	 * @param vmax
+	 *            - the absolute maximum allowed velocity
+	 * @param amax
+	 *            - the absolute maximum allowed acceleration
+	 * @param wmax
+	 *            - the absolute maximum allowed angular velocity
+	 * @param alphamax
+	 *            - the absolute maximum allowed angular acceleration
+	 * @param points
+	 *            - the number of points to sample along the trajectory
+	 */
+	public Trajectory(Path path, double v0, double v1, double vmax, double amax, double wmax, double alphamax, double jerkmax,
+			int points) {
+		this.path = path;
+		this.vmax = vmax;
+		this.amax = amax;
+		this.wmax = wmax;
+		this.alphamax = alphamax;
+		this.jerkmax = jerkmax;
+		velocities = new double[points];
+		velocities[0] = v0;
+		velocities[1] = v0;
+		velocities[points - 1] = v1;
+		acceleration = new double[points];
+		acceleration[0] = 0;
+		acceleration[points - 1] = 0;
+		computeForwardTrajectory();
+		computeReverseTrajectory();
+	}
+
+	/**
+	 * Numerically computes the optimal velocity value at each point on the
+	 * trajectory
+	 */
+	private void computeForwardTrajectory() {
+		for (int i = 1; i < velocities.length - 1; i++) {
+			double vprev = velocities[i - 1];
+			double aprev = acceleration[i - 1];
+			double vmax = getVmax(i);
+			double amax = Math.min(getAmax(i, i + 1, vprev), aprev + jerkmax);
+//			if (vprev + amax < vmax) {
+//				velocities[i] = vprev + amax;
+//			} else {
+//				velocities[i] = vmax;
+//			}
+			acceleration[i] = Math.min(amax, aprev + jerkmax);
+			velocities[i] = Math.min(vprev + amax, vmax);
+		}
+	}
+
+	/**
+	 * Computes the trajectory in reverse (call this AFTER
+	 * computeForwardTrajectory) Overwrites any infeasible velocity values that
+	 * were computed by the forward trajectory
+	 */
+	private void computeReverseTrajectory() {
+		for (int i = velocities.length - 2; i > 0; i--) {
+			double vprev = velocities[i + 1];
+			double aprev = acceleration[i + 1];
+			double vmax = getVmax(i);
+			double amax = Math.min(getAmax(i, i - 1, vprev), aprev + jerkmax);
+//			if (vprev + amax < vmax) {
+//				velocities[i] = Math.min(velocities[i], vprev + amax);
+//			} else {
+//				velocities[i] = Math.min(velocities[i], vmax);
+//			}
+			acceleration[i] = Math.min(amax, aprev + jerkmax);
+			velocities[i] = Math.min(vprev + amax, vmax);
+		}
+	}
+
+	/**
+	 * Gets the maximum velocity allowed at a given point
+	 * 
+	 * @param i
+	 *            - the point being evaluated
+	 * @return the maximum allowed velocity
+	 */
+	private double getVmax(int i) {
+		// point on the spline function
+		double s = 1.0 * i / velocities.length;
+		// dtheta/dL (not with respect to t)
+		double w = Math.abs(path.getW(s));
+		// d^2theta/dL^2 (not with respect to t)
+		double alpha = Math.abs(path.getAlpha(s)) + .000001; // Prevent division
+																// by zero
+		// velocity limit due to tradeoff between linear and angular velocity
+		double v1 = vmax / (1.0 + w * vmax / wmax);
+		// velocity limit due to maximum angular acceleration
+		double v2 = Math.sqrt(alphamax / alpha);
+		// return the smallest velocity limit
+		return Math.min(v1, v2);
+	}
+
+	/**
+	 * Gets the maximum acceleration allowed at a given point and velocity
+	 * 
+	 * @param i1
+	 *            - the point being evaluated
+	 * @param i2
+	 *            - the next point on the path
+	 * @param v
+	 *            - the current velocity
+	 * @return the maximum allowed acceleration (accurate to 3 decimal places)
+	 */
+	private double getAmax(int i1, int i2, double v) {
+		// old point on the spline function
+		double s0 = 1.0 * (i1) / velocities.length;
+		// new point on the spline function
+		double s1 = 1.0 * (i2) / velocities.length;
+		// change in arc length between the two points
+		double l = Math.abs(s1 - s0) * v;
+		// initial dw/dL
+		double w0 = Math.abs(path.getW(s0));
+		// final dw/dL
+		double w1 = Math.abs(path.getW(s1));
+		// acceleration limit due to tradeoff between linear and angular
+		// acceleration
+		double a = amax / 2;
+		for (int j = 2; j <= Math.log(1000) / Math.log(2) + 1; j++) {
+			double sqrt = Math.sqrt(v * v + 2 * a * l);
+			double alpha = Math.abs((w1 * a * sqrt - w0 * a * v) / (-v + sqrt));
+			if (a < amax - amax * alpha / alphamax) {
+				a += amax / Math.pow(2, j);
+			} else {
+				a -= amax / Math.pow(2, j);
+			}
+		}
+		return a;
+	}
+
+	/**
+	 * Gets the target velocity for a given point
+	 * 
+	 * @param i
+	 *            - the index of the point
+	 * @return the target velocity
+	 */
+	public double getV(int i) {
+		return velocities[i];
+	}
+
+	/**
+	 * Gets the target angular velocity for a given point
+	 * 
+	 * @param i
+	 *            - the index of the point
+	 * @return the target angular velocity
+	 */
+	public double getW(int i) {
+		double s = 1.0 * i / velocities.length;
+		return path.getW(s) * velocities[i];
+	}
+
+	/**
+	 * Determines the index of a point on the path
+	 * 
+	 * @param distance
+	 *            - the distance traveled along the path
+	 * @return the index of the current point
+	 */
+	public int getCurrentIndex(double distance) {
+		return (int) (path.getS(distance) * velocities.length);
+	}
+}
