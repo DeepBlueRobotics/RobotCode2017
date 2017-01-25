dde13ec21ce08b2bf5c45e602bd85509db15e028
caengineers <caengineers@ca-studentlabs.org>
2017-01-25 15:16:03 -0800

    formatted testPIDTest by david :)

--- Robot2017/src/tests/TestPIDTest.java
+++ Robot2017/src/tests/TestPIDTest.java
@@ -17,66 +17,66 @@ public class TestPIDTest {
 
 		TestPID testShooter = new TestPID(TestPID.System.SHOOTER, mockShoot, mockDrive);
 		testShooter.initialize();
-		
+
 		verify(mockShoot).setShooterPIDTarget(0);
 		verify(mockShoot).updateShooterPID(mockShoot.currentSpeed());
-		
+
 		testShooter.execute();
-		
+
 		verify(mockShoot).runShootMotor(mockShoot.updateSpeed(0));
-		
+
 		testShooter.end();
-		
+
 		verify(mockShoot).runShootMotor(0.0);
-		
+
 		TestPID testDriveDist = new TestPID(TestPID.System.DRIVEDISTANCE, mockShoot, mockDrive);
 		testDriveDist.initialize();
-		
+
 		verify(mockDrive).setDistanceTarget(0);
 		verify(mockDrive).setAngleTarget(0);
-		
+
 		testDriveDist.execute();
-		
+
 		verify(mockDrive).autoDrive();
-		
+
 		testDriveDist.isFinished();
-		
+
 		verify(mockDrive).distanceReachedTarget();
 		verify(mockDrive).angleReachedTarget();
-		
+
 		testDriveDist.end();
-		
+
 		verify(mockDrive).stopDrive();
-		
+
 		TestPID testDriveAng = new TestPID(TestPID.System.DRIVEANGLE, mockShoot, mockDrive);
 		testDriveAng.initialize();
-		
+
 		verify(mockDrive).setAngleTarget(0);
-		
+
 		testDriveAng.execute();
 		verify(mockDrive).updateAngle();
-		
+
 		testDriveAng.isFinished();
 		verify(mockDrive).angleReachedTarget();
-		
+
 		TestPID testDriveVel = new TestPID(TestPID.System.DRIVEVELOCITY, mockShoot, mockDrive);
 		testDriveVel.initialize();
-		
+
 		verify(mockDrive).setVelocityTarget(0, 0);
-		
+
 		testDriveVel.execute();
-		
+
 		verify(mockDrive).updateVelocity();
-		
+
 		TestPID testDriveAngVel = new TestPID(TestPID.System.DRIVEANGULARVELOCITY, mockShoot, mockDrive);
 		testDriveAngVel.initialize();
-		
+
 		verify(mockDrive).setVelocityTarget(0, 0);
-		
+
 		testDriveAngVel.execute();
-		
+
 		verify(mockDrive).updateVelocity();
-		
+
 	}
 
 }
