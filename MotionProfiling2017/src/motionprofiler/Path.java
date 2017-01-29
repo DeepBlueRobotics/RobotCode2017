f2169d0cc6e680e65dbad1b6157366e9480ad5e9
caengineers <caengineers@ca-studentlabs.org>
2017-01-28 16:05:35 -0800

    Integrated Motion Profiling and velocity PID into our code - Sammy

--- MotionProfiling2017/src/motionprofiler/Path.java
+++ MotionProfiling2017/src/motionprofiler/Path.java
@@ -125,7 +125,7 @@ public class Path {
 	 */
 	public double getL(double s) {
 		double l = 0;
-		double ds = 0.001;
+		double ds = 0.0001;
 		for(double i = 0; i<s; i+=ds) {
 			l+=getV(s)*ds;
 		}
--- Robot2017/src/org/usfirst/frc199/Robot2017/subsystems/Drivetrain.java
+++ Robot2017/src/org/usfirst/frc199/Robot2017/subsystems/Drivetrain.java
@@ -19,7 +19,7 @@ import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Talon;
-
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -41,6 +41,16 @@ public class Drivetrain extends Subsystem implements DrivetrainInterface {
 
 	private final PowerDistributionPanel pdp = RobotMap.pdp;
 
+	private PID distancePID = new PID("DriveDistance");
+	private PID anglePID = new PID("DriveAngle");
+	private PID velocityPID = new PID("DriveVelocity");
+	private PID angularVelocityPID = new PID("DriveAngularVelocity");
+
+	// Variables for motion profiling and acceleration control
+	private double prevEncoderRate = 0, prevGyroRate = 0, prevTime = 0, driveLimit = 0, turnLimit = 0;
+	private double gyroCalibrationInitalValue = 0, gyroDriftRate = 0;
+	private Timer gyroDriftTimer = new Timer();
+
 	public boolean isInArcadeDrive = true;
 	private double currentSpeed = 0; // only used and changed in Arcade Drive
 	private double currentTurn = 0;
@@ -56,20 +66,22 @@ public class Drivetrain extends Subsystem implements DrivetrainInterface {
 		setDefaultCommand(new TeleopDrive(Robot.drivetrain));
 
 	}
-	
+
 	/**
 	 * Gets the drivePID object
+	 * 
 	 * @return the drivePID object
-	 * */
-	public PID getDrivePID(){
+	 */
+	public PID getDrivePID() {
 		return drivePID;
 	}
-	
+
 	/**
-	 * Gets the turnPID object 
+	 * Gets the turnPID object
+	 * 
 	 * @return the turnPID object
-	 * */
-	public PID getTurnPID(){
+	 */
+	public PID getTurnPID() {
 		return turnPID;
 	}
 
@@ -224,7 +236,7 @@ public class Drivetrain extends Subsystem implements DrivetrainInterface {
 		SmartDashboard.putNumber("Turn Speed", gyro.getRate());
 
 		SmartDashboard.putBoolean("high gear", false);
-		
+
 		SmartDashboard.putBoolean("gear has been lifted", false);
 	}
 
@@ -258,25 +270,102 @@ public class Drivetrain extends Subsystem implements DrivetrainInterface {
 
 	/**
 	 * Sets targets for tracking velocity of robot for motion profiling
+	 * 
 	 * @param linVelTarget
 	 * @param angVelTarget
 	 */
 	public void setVelocityTarget(double linVelTarget, double angVelTarget) {
-		// TODO (Ana T.) Write method for setting target of velocity PID. Check last
+		// TODO (Ana T.) Write method for setting target of velocity PID. Check
+		// last
 		// year's code cause that looked more complicated than it needs to be.
 		// Figure out why
 	}
-	
-	public boolean gearLifted(){
-		//return if gear lifted or not
+
+	public boolean gearLifted() {
+		// return if gear lifted or not
 		return true;
 	}
-	
+
 	/**
 	 * Updates linear and angular velocity PIDs for motion profiling
 	 */
 	public void updateVelocity() {
 		// TODO (Ana T.) See todo of setVelocityTarget method
-		
+
+	}
+
+	/**
+	 * Uses PID to reach target velocity
+	 * 
+	 * @param v
+	 *            - linear velocity in inches/second
+	 * @param w
+	 *            - angular velocity in degrees/second
+	 * @param a
+	 *            - acceleration in inches/second/second
+	 * @param alpha
+	 *            - angular acceleration in degrees/second/second
+	 */
+	public void followTrajectory(double v, double w, double a, double alpha) {
+		velocityPID.setTarget(v);
+		angularVelocityPID.setTarget(w);
+		velocityPID.update(getEncoderRate());
+		angularVelocityPID.update(getGyroRate());
+
+		double kV = 1.0 / Robot.getPref("DriveMaxV", .01);
+		double kW = 1.0 / Robot.getPref("DriveMaxW", .01);
+		double kA = SmartDashboard.getNumber("MotionProfile/kA", Robot.getPref("DrivekA", 0.0));
+		double kAlpha = SmartDashboard.getNumber("MotionProfile/kAlpha", Robot.getPref("DrivekAlpha", 0.0));
+
+		double outputV = velocityPID.getOutput() + kV * v + kA * a; // Computes
+																	// Output
+																	// Velocity
+																	// Using PID
+		double outputW = angularVelocityPID.getOutput() + kW * w + kAlpha * alpha; // Computes
+																					// Output
+																					// Angular
+																					// Velocity
+																					// Using
+																					// PID
+
+		arcadeDrive(outputV, outputW);
+
+		SmartDashboard.putNumber("MotionProfile/L", getEncoderDistance());
+		SmartDashboard.putNumber("MotionProfile/V", getEncoderRate());
+		SmartDashboard.putNumber("MotionProfile/A",
+				(getEncoderRate() - prevEncoderRate) / (Timer.getFPGATimestamp() - prevTime));
+		SmartDashboard.putNumber("MotionProfile/Theta", getGyroAngle());
+		SmartDashboard.putNumber("MotionProfile/W", getGyroRate());
+		SmartDashboard.putNumber("MotionProfile/Alpha",
+				(getGyroRate() - prevGyroRate) / (Timer.getFPGATimestamp() - prevTime));
+		prevEncoderRate = getEncoderRate();
+		prevGyroRate = getGyroRate();
+		prevTime = Timer.getFPGATimestamp();
+	}
+
+	public void arcadeDrive(double speed, double turn) {
+		// TODO Auto-generated method stub
+		robotDrive.arcadeDrive(speed, -turn);
+	}
+
+	private double getGyroAngle() {
+		// TODO Auto-generated method stub
+		return gyro.getAngle() - gyroDriftRate * gyroDriftTimer.get();
+
+	}
+
+	public double getEncoderDistance() {
+		// TODO Auto-generated method stub
+		return (-leftEncoder.getDistance() + rightEncoder.getDistance()) / 2;
+	}
+
+	private double getGyroRate() {
+		// TODO Auto-generated method stub
+		return gyro.getRate() - gyroDriftRate;
+	}
+
+	private double getEncoderRate() {
+		// TODO Auto-generated method stub
+		return (leftEncoder.getRate() + rightEncoder.getRate()) / 2;
 	}
 }
