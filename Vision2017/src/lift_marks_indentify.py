f91b1ccd0bb6c38867b6d2a98fc69f216e31bde5
Kevin Wang <kevinzhiwang@gmail.com>
2017-02-03 17:45:30 -0800

    finished python part of gear vision
    "you're welcome" - kevin (that's me)

--- Vision2017/src/lift_marks_indentify.py
+++ Vision2017/src/lift_marks_indentify.py
@@ -5,46 +5,61 @@ Must return the centers of both strips of tape
 
 import numpy as np
 import cv2
+import operator
 
-def findTape(cap, lowerRange, upperRange):
-    #Structure all of this as you think best
-    cap = cv2.VideoCapture(camNum)
-     
-    center1 = [0, 0]
-    center2 = [0, 0]
+def findTape(cap, lowerHSV, upperHSV):
     
-    for i in range(0, 5):
-        ret, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lowerHSV, upperHSV)
+    
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[1]
+    
+    # cntAreas = []
+    # centerX = []
+    # centerY = []
+
+    # contains tuples of contours in format: (area, x-val of center, y-val of center)
+    cnts2 = []
+
+    for c in cnts:
+        epsilon = 0.05 * cv2.arcLength(c,True)
+        c = cv2.approxPolyDP(c, epsilon, True)
         
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(hsv, lowerRange, upperRange)
+        area = cv2.contourArea(c)
         
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[1]
+        minRect = cv2.minAreaRect(c)
+        minRectArea = cv2.contourArea(minRect)
         
-        
-        
-        for c in cnts:
-            epsilon = 0.05*cv2.arcLength(c,True)
-            c = cv2.approxPolyDP(c,epsilon,True)
-            
-            contourArea = cv2.contourArea(c)
-            
-            minRect = cv2.minAreaRect(c)
-            minRectArea = cv2.contourArea(minRect)
-            
-            # checks if the thing is a rectangle
-            if (len(c) == 4) and ((minRectArea * 0.9) < contourArea):
-                M = cv2.moments(c)
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
-                
-            
-            
-            
-            
-            
-            
-            
-            
-        
-        
+        # checks if the thing is a rectangle, then records area and centers if true
+        if (len(c) == 4) and ((minRectArea * 0.9) < area):
+
+            # Past self: "WTF are moments?!" Later past self: "Chill, don't worry about it, just assume it works."
+            M = cv2.moments(c)
+            centerX = int(M["m10"] / M["m00"])
+            centerY = (int(M["m01"] / M["m00"])
+
+            # tup = (len(centerX) - 1, area)
+            # cntAreas.append(area)
+
+            tup = (area, centerX, centerY)
+            cnts2.append(tup)
+
+    # sorts contours by largest to smallest area
+    cnts2.sort(key=operator.itemgetter(0))
+    cnts2.reverse()
+
+    for i in range(0, len(cnts2) - 1):
+        for j in range(i + 1, len(cnts2) - 1):
+
+            # if this contour's area is too small, there's no use in looking through the rest because they're sorted by area
+            if cnts2[i][0] * 0.8 < cnts2[j][0]:
+                break
+
+            # if they line up, it fills the final requirement and yey. Returns.
+            else if (cnts2[i][2] - 15 <= cnts2[j][2]) and (cnts2[i][2] + 15 >= cnts2[j][2]):
+                if cnts2[i][1] > cnts2[j][1]:
+                    return cnts2[j][1], cnts2[j][2], cnts2[i][1], cnts2[i][2], True
+                else:
+                    return cnts2[i][1], cnts2[i][2], cnts2[j][1], cnts2[j][2], True
+
+    return -1, -1, -1, -1, False
