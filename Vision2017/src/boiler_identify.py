a64c38bc7f4977fa6ba8bd19249fcc7d4c5947b8
kevin <kevinzhiwang@gmail.com>
2017-02-14 17:37:01 -0800

    boiler vision donecd Documents/RobotCode2017/RobotCode2017/ woowoo

--- Vision2017/src/boiler_identify.py
+++ Vision2017/src/boiler_identify.py
@@ -7,22 +7,25 @@ import cv2
 import numpy as np
 
 def findBoiler(frame, lower, upper):
+        mask = cv2.inRange(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV), lower, upper)
 	# cnts: just the contours alone
-	cnts = cv2.findContours(cv2.inRange(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV), lower, upper), cv2.RETR_LIST,
-		cv2.CHAIN_APPROX_SIMPLE)[1]
+	cnts = cv2.findContours(mask,cv2.RETR_LIST,
+		cv2.CHAIN_APPROX_SIMPLE)[0]
 	
 	# vals contains (contour, area) of all contours
 	vals = []
 	
 	# loop over the contours
-	for c in cnts:
-		vals.append(c, cv2.contourArea(c))
+        for c in cnts:
+                vals.append((c, cv2.contourArea(c)))
 							
-	vals.sort(key=lambda x: x[1])	
+	vals.sort(key=lambda x: x[1], reverse=True)	
 	
 	# if the biggest two contours are still tiny, return all -1 
-	if (vals[1][1] < 5): # no tape found
+	if (len(vals) < 2): # no tape found
 		return (-1, -1)
+	elif (vals[1][1] < 5):
+                return (-1, -1)
 	else:
 		box = cv2.boundingRect(vals[0][0])
 		return (box[0] + box[2]/2, box[1])
