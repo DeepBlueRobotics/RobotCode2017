ac492282a08eb8d244d6dfb5088d008bed534bf5
David Fang <dfang314@gmail.com>
2017-02-20 10:53:58 -0800

    look it works now autodrivetest by david :D

---
+++ Robot2017/src/tests/AutoDriveTestThatWorks.java
@@ -0,0 +1,33 @@
+package tests;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.usfirst.frc199.Robot2017.commands.AutoDrive;
+import org.usfirst.frc199.Robot2017.subsystems.DrivetrainInterface;
+
+public class AutoDriveTestThatWorks {
+
+	@Test
+	public void test() {
+		DrivetrainInterface drivetrain = mock(DrivetrainInterface.class);
+		AutoDrive autoDrive = new AutoDrive(0, 0, drivetrain);
+		
+		autoDrive.initialize();
+		verify(drivetrain).resetEncoder();
+		verify(drivetrain).resetGyro();
+		verify(drivetrain).setDistanceTarget(0);
+		verify(drivetrain).setAngleTarget(0);
+		
+		autoDrive.execute();
+		verify(drivetrain).autoDrive();
+		verify(drivetrain).currentControl();
+		
+		autoDrive.isFinished();
+		verify(drivetrain).distanceReachedTarget();
+		
+		autoDrive.end();
+		verify(drivetrain).stopDrive();
+	}
+
+}
