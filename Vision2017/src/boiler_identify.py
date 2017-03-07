e949a627745a0c30ed89943a75e7a21025aecbe9
Gabe Mitnick <gabe.mitnick@gmail.com>
2017-03-06 17:21:21 -0800

    i hate python
    the tabs were pǝʞɔnɟ up. fix them.

--- Vision2017/src/boiler_identify.py
+++ Vision2017/src/boiler_identify.py
@@ -7,7 +7,7 @@ import cv2
 import numpy as np
 
 def findBoiler(frame, lower, upper):
-        mask = cv2.inRange(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV), lower, upper)
+	mask = cv2.inRange(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV), lower, upper)
 	# cnts: just the contours alone
 	cnts = cv2.findContours(mask,cv2.RETR_LIST,
 		cv2.CHAIN_APPROX_SIMPLE)[0]
@@ -16,17 +16,15 @@ def findBoiler(frame, lower, upper):
 	vals = []
 	
 	# loop over the contours
-        for c in cnts:
-                vals.append((c, cv2.contourArea(c)))
+	for c in cnts:
+		vals.append((c, cv2.contourArea(c)))
 							
 	vals.sort(key=lambda x: x[1], reverse=True)	
 	
-	# if the biggest two contours are still tiny, return all -1 
-	if (len(vals) < 2): # no tape found
+	# if there are fewer than 2 contours or the biggest two contours are still tiny, return all -1
+	if (len(vals) < 2 or (vals[1][1] < 5):
 		return (-1, -1)
-	elif (vals[1][1] < 5):
-                return (-1, -1)
 	else:
 		box = cv2.boundingRect(vals[0][0])
 		return (box[0] + box[2]/2, box[1])
-	#               x  +  width/2,     y
+	#			   x  +  width/2,	 y
