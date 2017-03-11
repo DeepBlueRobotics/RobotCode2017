257e6aad2b739b05d45a3f7ca1cec56215053279
Orli Feuchtwang <orli.feuchtwang@gmail.com>
2017-03-11 10:24:31 -0800

    Changed auto modes for testing
    Reimplemented autoaligngear for testing

--- Robot2017/src/org/usfirst/frc199/Robot2017/commands/AutoModeLoadSide.java
+++ Robot2017/src/org/usfirst/frc199/Robot2017/commands/AutoModeLoadSide.java
@@ -69,10 +69,10 @@ public class AutoModeLoadSide extends CommandGroup {
 		addSequential(new AutoDrive(0, direction * 60, Robot.drivetrain));
 
 		// Drive Forward
-		addSequential(new AutoDrive(LENGTH_2, 0, Robot.drivetrain));
+		//addSequential(new AutoDrive(LENGTH_2, 0, Robot.drivetrain));
 		
 		// drives up to lift and aligns
-		//addSequential(new AutoAlignGear(false));
+		addSequential(new AutoAlignGear(false));
 
 		// METHOD 2:
 		/*
