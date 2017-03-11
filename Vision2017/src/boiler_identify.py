17f0da3e58a7326539a61a5fab74438229acfc9d
Kevin Wang <kevinzhiwang@gmail.com>
2017-03-10 19:23:09 -0800

    finish!
    made selection of the correct contours more specific by checking if two
    contours have an area ratio of 1:2 and if their center x values are
    similar

--- Vision2017/src/boiler_identify.py
+++ Vision2017/src/boiler_identify.py
@@ -19,14 +19,27 @@ def findBoiler(frame, lower, upper):
 	for c in cnts:
 		area = cv2.contourArea(c)
 		if (area > 5 and area < 105):
-			vals.append((c, area))
+			box = cv2.boundingRect(c)
+			vals.append((area, box[0] + box[2]/2, box[1]))
 
-	vals.sort(key=lambda x: x[1], reverse=True)	
+	vals.sort(key=lambda x: x[0], reverse=True)	
+
+	for i in range[0, len(vals) - 2]:
+		for j in range[i, len(vals) - 1]:
+			# give up on the rest of area ratio is less than 1/2 because it's sorted by size
+			if (vals[i][0] * (2/5) > vals[j][0]):
+				break
+
+			# if area ratio is around 1/2 and the center x values are around the same, it's a match!
+			if (vals[i][0] * (3/5) > vals[j][0] and vals[i][1] + 50 > vals[j][1] and vals[i][1] - 50 < vals[j][1]):
+				return (vals[i][1], vals[i][2])
+
+	return (-1, -1)
 	
 	# if there are fewer than 2 contours or the biggest two contours are still tiny, return all -1
-	if (len(vals) < 2):
-		return (-1, -1)
-	else:
-		box = cv2.boundingRect(vals[0][0])
-		return (box[0] + box[2]/2, box[1])
-	#			   x  +  width/2,	 y
+	# if (len(vals) < 2):
+	# 	return (-1, -1)
+	# else:
+	# 	box = cv2.boundingRect(vals[0][0])
+	# 	return (box[0] + box[2]/2, box[1])
+	# #			   x  +  width/2,	 y
