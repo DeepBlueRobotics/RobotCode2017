622331c14bfd6e161fdb6e5c81ac2a53e0c6a501
Kevin Wang <kevinzhiwang@gmail.com>
2017-03-11 15:21:32 -0800

    Resolved Vision.java conflicts

--- Robot2017/src/org/usfirst/frc199/Robot2017/subsystems/Vision.java
+++ Robot2017/src/org/usfirst/frc199/Robot2017/subsystems/Vision.java
@@ -5,30 +5,7 @@ import org.usfirst.frc199.Robot2017.Robot;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class Vision extends Subsystem implements DashboardInterface {
-<<<<<<< HEAD
-
-	private final double REFLECTOR_DIST_GEAR = 5; // inches between the centers
-													// of both reflectors
-													// (horizontal)
-	private final double SHOOTER_CAM_HEIGHT = Robot.getPref("Camera Height", 0); // in
-																			// off
-																			// the
-																			// ground
-	private final double BOILER_HEIGHT = 88 - SHOOTER_CAM_HEIGHT; // in from camera
-															// bottom to top of
-															// boiler tape
-	private final double THETA = 34.25 * (Math.PI / 180); // radians from north
-															// that you can see
-															// in both
-															// directions
-	private final double SHOOTER_CAM_ANGLE = 41.91 * (Math.PI / 180); // radians of the
-																// camera's
-																// vertical
-																// field of view
-	private final double RESOLUTION_WIDTH = 640; // pixel width of image
-	private final double RESOLUTION_HEIGHT = 360; // pixel height of image
-=======
+public class Vision extends Subsystem implements DashboardInterface { 
 	// inches between the centers of both reflectors (horizontal)
 	private final double REFLECTOR_DIST_GEAR = 8.25;
 	// inches off the ground (~21.67)
@@ -43,8 +20,8 @@ public class Vision extends Subsystem implements DashboardInterface {
 	private final double RESOLUTION_WIDTH = 640; 
 	// pixel height of image
 	private final double RESOLUTION_HEIGHT = 360;
->>>>>>> a0dc76e1da27491723489b4c765017d1afa592db
 	private double SCREEN_DEPTH = RESOLUTION_HEIGHT / Math.tan(SHOOTER_CAM_ANGLE);
+
 	private final double SCREEN_CENTER_X = RESOLUTION_WIDTH / 2;
 	private final double SCREEN_CENTER_Y = RESOLUTION_HEIGHT / 2;
 	
@@ -70,11 +47,7 @@ public class Vision extends Subsystem implements DashboardInterface {
 //			double pixelDist = Math.abs(getPixelDistanceToGear());
 			double fieldOfView = (REFLECTOR_DIST_GEAR * RESOLUTION_WIDTH) / pixelDist;
 			double distanceToGear = (fieldOfView / 2) / (Math.tan(THETA));
-<<<<<<< HEAD
-			return 2 * distanceToGear;
-=======
 			return distanceToGear; 
->>>>>>> a0dc76e1da27491723489b4c765017d1afa592db
 		} else {
 			return 0;
 		}
@@ -87,20 +60,11 @@ public class Vision extends Subsystem implements DashboardInterface {
 	public double getPixelDistanceToGear() {
 		if (getBoolean("OH-YEAH", true)) {
 			double pegX = (getNumber("leftGearCenterX", 0) + getNumber("rightGearCenterX", 0)) / 2;
-<<<<<<< HEAD
-			double pixelDisplacement = pegX - getNumber("gearCenterX", 0); // SCREEN_CENTER;
-			double abstractDepth = (RESOLUTION_WIDTH / 2) / Math.tan(THETA);
-
-			double angle = (Math.atan(pixelDisplacement / abstractDepth) * 180) / Math.PI;
-
-			return angle;
-=======
 			double pegY = (getNumber("leftGearCenterY", 0) + getNumber("rightGearCenterY", 0)) / 2;
 			double m = (getNumber("rightGearCenterY", 0) - getNumber("leftGearCenterY", 0)) / 
 					(getNumber("rightGearCenterX", 0) - getNumber("leftGearCenterX", 0));
 	
 			return ( -pegX/m + pegY - SCREEN_CENTER_Y + SCREEN_CENTER_X/m ) / Math.sqrt(1/(m*m) + 1);
->>>>>>> a0dc76e1da27491723489b4c765017d1afa592db
 		} else {
 			return 0;
 		}
