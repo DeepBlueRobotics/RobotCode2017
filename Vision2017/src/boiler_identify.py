70993bc044ff33e9e668615b087f19a17b482dda
Kevin Wang <kevinzhiwang@gmail.com>
2017-03-11 16:57:58 -0800

    Revert "accidentally commited merge conflict lol"
    This reverts commit 8c39db67cd48fe034f19bc249fbdfef9c56824c5.

--- Vision2017/src/boiler_identify.py
+++ Vision2017/src/boiler_identify.py
@@ -16,7 +16,15 @@ def findBoiler(frame, lower, upper):
 	vals = []
 	
 	# loop over the contours
+<<<<<<< HEAD
+        for c in cnts:
+                vals.append((c, cv2.contourArea(c)))
+							
+	vals.sort(key=lambda x: x[1], reverse=True)	
 	
+	# if the biggest two contours are still tiny, return all -1 
+	if (len(vals) < 2): # no tape found
+=======
 	for c in cnts:
 		area = cv2.contourArea(c)
 		if (area > 5 and area < 105):
@@ -26,7 +34,7 @@ def findBoiler(frame, lower, upper):
 	
 	# if there are fewer than 2 contours or the biggest two contours are still tiny, return all -1
 	if (len(vals) < 2):
-
+>>>>>>> a0dc76e1da27491723489b4c765017d1afa592db
 		return (-1, -1)
 	elif (vals[1][1] < 5):
                 return (-1, -1)
