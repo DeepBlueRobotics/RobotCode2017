209f4499c3344c59f75a481be4599af3659f9fec
David Fang <dfang314@gmail.com>
2017-03-17 13:20:09 -0700

    made it less effective against toggling type by david :D

--- Robot2017/src/org/usfirst/frc199/Robot2017/commands/NeverEndTheIntaking.java
+++ Robot2017/src/org/usfirst/frc199/Robot2017/commands/NeverEndTheIntaking.java
@@ -47,7 +47,7 @@ public class NeverEndTheIntaking extends Command {
 
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-		return false;
+		return tim.get() > 1 && intake.intakeIsDown();
 	}
 
 	// Called once after isFinished returns true
