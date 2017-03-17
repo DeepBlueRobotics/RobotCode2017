11574cab6aebeed7a8bad26d9a612c8f9a89fd50
Gabe Mitnick <gabe.mitnick@gmail.com>
2017-03-17 14:04:34 -0700

    another try

--- Robot2017/src/org/usfirst/frc199/Robot2017/Robot.java
+++ Robot2017/src/org/usfirst/frc199/Robot2017/Robot.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc199.Robot2017;
 
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Preferences;
@@ -10,6 +13,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import java.util.ArrayList;
 
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 import org.usfirst.frc199.Robot2017.DashboardInterface;
 import org.usfirst.frc199.Robot2017.commands.*;
 import org.usfirst.frc199.Robot2017.subsystems.*;
@@ -39,7 +44,6 @@ public class Robot extends IterativeRobot {
 	 */
 	public void robotInit() {
 		RobotMap.init();
-		CameraServer.getInstance().startAutomaticCapture();
 		drivetrain = new Drivetrain();
 		intake = new Intake();
 		shooter = new Shooter();
@@ -68,6 +72,22 @@ public class Robot extends IterativeRobot {
 		SmartDashboard.putData(Scheduler.getInstance());
 		SmartDashboard.putBoolean("Vision/gearRunning", false);
 		SmartDashboard.putBoolean("Vision/shooterRunning", false);
+		new Thread(() -> {
+            UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+            camera.setResolution(640, 480);
+            
+            CvSink cvSink = CameraServer.getInstance().getVideo();
+            CvSource outputStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
+            
+            Mat source = new Mat();
+            Mat output = new Mat();
+            
+            while(!Thread.interrupted()) {
+                cvSink.grabFrame(source);
+                Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
+                outputStream.putFrame(output);
+            }
+        }).start();
 	}
 
 	/**
