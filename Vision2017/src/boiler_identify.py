a85b2ff20f89fd9d06b27109fa6c2c4698cff054
Gabe Mitnick <gabe.mitnick@gmail.com>
2017-03-21 17:47:25 -0700

    jfc kevin
    fix kevin's shitty indentation and re-add the max area in
    boiler_identify that kevin took out for <who tf knows why>

--- Vision2017/src/boiler_identify.py
+++ Vision2017/src/boiler_identify.py
@@ -9,23 +9,23 @@ import numpy as np
 def findBoiler(frame, lower, upper):
 	mask = cv2.inRange(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV), lower, upper)
 	# cnts: just the contours alone
-        cnts = cv2.findContours(mask,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)[0]
-	
+	cnts = cv2.findContours(mask,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)[0]
+
 	# vals contains (contour, area) of all contours
 	vals = []
-	
+
 	# loop over the contours
-        for c in cnts:
-                vals.append((c, cv2.contourArea(c)))
-							
-	vals.sort(key=lambda x: x[1], reverse=True)	
-	
-	# if the biggest two contours are still tiny, return all -1 
-	if (len(vals) < 2): # no tape found
-		return (-1, -1)
-	elif (vals[1][1] < 5):
+		for c in cnts:
+			area = cv2.contourArea(c)
+			if (area > 5 && area < 105):
+				vals.append((c, area))
+
+	vals.sort(key=lambda x: x[1], reverse=True)
+
+	# if the biggest two contours are still tiny, return all -1
+	if (len(vals) < 2):
 		return (-1, -1)
 	else:
 		box = cv2.boundingRect(vals[0][0])
+		# x + width/2, y
 		return (box[0] + box[2]/2, box[1])
-	#			   x  +  width/2,	 y
